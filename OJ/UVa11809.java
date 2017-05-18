49cd0422396fc4ab88d960fafd13af23a616f9c4
scruel <scruel@vip.qq.com>
2017-05-18 10:25:35 +0800

    upload algs codes

--- OJ/UVa11809.java
+++ OJ/UVa11809.java
@@ -1,28 +1,53 @@
 package algsPractice.OJ;
 
+import java.io.*;
+
 /**
  * Created by Scruel on 2017/4/13.
  * Personal blog : http://blog.csdn.net/scruelt
  * Github : https://github.com/scruel
- * #binary
+ * http://blog.csdn.net/crazysillynerd/article/details/43339157
+ * #binary math int --> log
  */
 public class UVa11809 {
-        static long solve(String s) {
-                long sum = 0;
-                long powSum = 1;
-                for (int i = s.length() - 1; i >= 0; i--) {
-                        if (s.charAt(i) == '1')
-                                sum += powSum;
-                        powSum *= 2;
+
+        static final double EPSINON = 1e-4;
+        static BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
+        static BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out), 1 << 16);
+        static double[][] M = new double[20][40];
+        static long[][] E = new long[20][40];
+
+        public static void main(String[] args) throws IOException {
+                String ts;
+                for (int i = 0; i <= 9; i++) {
+                        for (int j = 1; j <= 30; j++) {
+                                double m = 1.0 - Math.pow(2, -1 - i);
+                                double e = (1 << j) - 1;
+                                double t = (Math.log10(m) + e * Math.log10(2));
+                                E[i][j] = (long) t;
+                                M[i][j] = Math.pow(10, t - E[i][j]);
+                        }
+                }
+                while ((ts = bfr.readLine()) != null && !"0e0".equals(ts)) {
+                        String[] rts = ts.toLowerCase().split("e");
+                        double A = Double.parseDouble(rts[0]);
+                        long B = Long.parseLong(rts[1]);
+                        while (A < 1) {
+                                A *= 10;
+                                B -= 1;
+                        }
+                        for (int i = 0; i <= 9; i++) {
+                                for (int j = 1; j <= 30; j++) {
+                                        if (B == E[i][j] && (Math.abs(A - M[i][j]) < EPSINON || Math.abs(A / 10 - M[i][j]) < EPSINON)) {
+                                                bfw.write(String.format("%d %d\n", i, j));
+                                                break;
+                                        }
+                                }
+                        }
+
                 }
 
-                return sum;
-        }
-
-        public static void main(String[] args) {
-                String temp = "1111111111111111111111111111111111111111111111111111111111";
-                System.out.println(solve(temp));
-                System.out.println(Long.parseLong(temp, 2));
-
+                bfr.close();
+                bfw.close();
         }
 }
--- OJ/UVa1587.java
+++ OJ/UVa1587.java
@@ -7,82 +7,61 @@ import java.util.*;
  * Created by Scruel on 2017/5/17.
  * Personal blog : http://blog.csdn.net/scruelt
  * Github : https://github.com/scruel
+ * #easy sort
  */
 public class UVa1587 {
         static BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
         static BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out), 1 << 16);
-        static Map<Box, Integer> map;
+
+        static Box getBox(String str) {
+                String[] rts = str.split("\\s+");
+                Box box = new Box();
+                int x = Integer.parseInt(rts[0]);
+                int y = Integer.parseInt(rts[1]);
+                if (x < y) {
+                        box.a = x;
+                        box.b = y;
+                } else {
+                        box.a = y;
+                        box.b = x;
+                }
+                return box;
+
+        }
 
         public static void main(String[] args) throws IOException {
 //                bfr = new BufferedReader(new InputStreamReader(DataReader.getDataReader()));
                 String ts;
                 while ((ts = bfr.readLine()) != null && !ts.isEmpty()) {
-                        map = new HashMap<Box, Integer>();
-                        String[] rts = ts.split("\\s+");
-                        Box box = new Box();
-                        box.a = Integer.parseInt(rts[0]);
-                        box.b = Integer.parseInt(rts[1]);
-                        map.put(box, 1);
+                        Box[] boxs = new Box[6];
+                        int cnt = 0;
+                        boxs[cnt++] = getBox(ts);
                         for (int i = 0; i < 5; i++) {
-                                rts = bfr.readLine().split("\\s+");
-                                box = new Box();
-                                box.a = Integer.parseInt(rts[0]);
-                                box.b = Integer.parseInt(rts[1]);
-                                if (map.containsKey(box))
-                                        map.put(box, map.get(box) + 1);
-                                else
-                                        map.put(box, 1);
+                                boxs[cnt++] = getBox(bfr.readLine());
                         }
-                        boolean ok;
-                        if (map.size() > 3) {
-                                ok = false;
-                        } else {
-                                boolean flag = true;
-                                Set<Map.Entry<Box, Integer>> ey = map.entrySet();
-                                LinkedList<Box> list = new LinkedList<Box>();
-                                for (Map.Entry<Box, Integer> entry : ey) {
-                                        list.add(entry.getKey());
-                                        if ((entry.getValue() & 1) == 1) {
-                                                flag = false;
-                                                break;
-                                        }
+                        boolean ok = true;
+                        Arrays.sort(boxs);
+                        //排序后检查规则，是否排成了ab ab ac ac bc bc这样两两相对的情况
+                        for (int i = 0; i < 6; i += 2) {
+                                if (boxs[i].a != boxs[i + 1].a || boxs[i].b != boxs[i + 1].b) {
+                                        ok = false;
+                                        break;
                                 }
-                                if (map.size() == 3 && flag) {
-                                        Box b1 = list.get(0);
-                                        Box b2 = list.get(1);
-                                        Box b3 = list.get(2);
-                                        int x = 0, y = 0;
-                                        if (b1.a == b2.a) {
-                                                x = b1.b;
-                                                y = b2.b;
-                                        } else if (b1.a == b2.b) {
-                                                x = b1.b;
-                                                y = b2.a;
-                                        } else if (b1.b == b2.a) {
-                                                x = b1.a;
-                                                y = b2.b;
-                                        } else if (b1.b == b2.b) {
-                                                x = b1.a;
-                                                y = b2.a;
-                                        }
-                                        ok = (b3.a == x && b3.b == y) || (b3.b == x && b3.a == y);
-
-
-                                } else {
-                                        ok = flag;
-                                }
+                        }
+                        //排序后必定按照ab、ac、bc的序列
+                        if (ok) {
+                                ok = (boxs[0].a == boxs[2].a && boxs[0].b == boxs[4].a &&
+                                        boxs[2].b == boxs[4].b);
 
                         }
                         bfw.write(ok ? "POSSIBLE\n" : "IMPOSSIBLE\n");
-
-
                 }
 
                 bfr.close();
                 bfw.close();
         }
 
-        static class Box {
+        static class Box implements Comparable<Box> {
                 int a, b;
 
                 @Override
@@ -94,6 +73,13 @@ public class UVa1587 {
                 @Override
                 public int hashCode() {
                         return a + b;
+                }
+
+                @Override
+                public int compareTo(Box o) {
+                        if (this.a > o.a) return 1;
+                        else if (this.a < o.a) return -1;
+                        else return this.b - o.b;
                 }
         }
 }
