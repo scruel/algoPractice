4bc6345bfdef2e1d0dd4cfe94bab8f9ed65234a9
scruel <scruel@vip.qq.com>
2017-12-16 22:47:02 +0800

    51nod 1307

--- oj/nod51/Task1307.java
+++ oj/nod51/Task1307.java
@@ -2,13 +2,58 @@ package algsPractice.oj.nod51;
 
 import algsPractice.lib.InputReader;
 import algsPractice.lib.OutputWriter;
-import static java.lang.Math.*;
+import com.sun.xml.internal.messaging.saaj.soap.ver1_1.BodyElement1_1Impl;
 
 /**
- * Created by scruel on 2017/12/16.
- * Github : https://github.com/scruel
+ * @author scruel
+ * <p>
+ * dfs 判断是否会断掉
+ * 方法一：二分计算转判断 (nlogn)
+ * 方法二：DFS时使用并查集
+ * 方法三：Tarjan Lca - A linera-time algorithm for a special case disjoint set union.
  */
 public class Task1307 {
-    public void solve(int testNumber, InputReader in, OutputWriter out) {
+  class Node {
+    int parent, root, weight, hold;
+    long sum;
+  }
+
+  public void solve(int testNumber, InputReader in, OutputWriter out) {
+    int n = in.nextInt();
+    Node[] nodes = new Node[n];
+    for (int i = 0; i < n; i++) {
+      Node node = new Node();
+      node.root = i;
+      node.hold = in.nextInt();
+      node.weight = in.nextInt();
+      node.sum = node.weight;
+      node.parent = in.nextInt();
+      nodes[i] = node;
     }
+
+    int max = n - 1;
+    for (int i = n - 1; i >= 0; i--) {
+      Node node = nodes[i];
+      while (node.hold < node.sum) {
+        // 超重了，开始从最后一根向上并查减少
+        int root = max, id = max;
+        while (nodes[root].root != root) {
+          root = nodes[root].root;
+        }
+        while (nodes[id].root != root) {
+          int cid = id;
+          id = nodes[id].root;
+          nodes[cid].root = root;
+        }
+        nodes[root].sum -= nodes[max].weight;
+        max--;
+      }
+
+      if (node.parent != -1) {
+        node.root = node.parent;
+        nodes[node.parent].sum += node.sum;
+      }
+    }
+    out.writeln(max + 1);
+  }
 }
